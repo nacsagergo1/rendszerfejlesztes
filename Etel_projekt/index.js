69016488edcd4652a6ad9eb39bbeb1c88838becf
Sánta Krisztina Csilla <h269825@stud.u-szeged.hu>
2024-11-01 18:19:23 +0100

    update dependencies

--- Etel_projekt/index.js
+++ Etel_projekt/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const path = require("path");
+const { Pool } = require("pg");
 
 const app = express();
 const PORT = process.env.port || 8080;
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
