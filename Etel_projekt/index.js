64c3218713b0446d475ab9f6ccc1c12e8fa0c160
rpeti <peter.rozsas@euroknows.com>
2024-11-08 12:59:08 +0100

    Routes fit Pt2

--- Etel_projekt/index.js
+++ Etel_projekt/index.js
@@ -2,30 +2,32 @@ const express = require("express");
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const path = require("path");
-const { Pool } = require("pg");
+const cookieParser = require("cookie-parser");
 const genRoutes = require('./routes/gen_routes');
 const userRoutes = require('./routes/user_routes');
 
 const app = express();
-const PORT = process.env.port || 8080;
-const cookieParser = require("cookie-parser");
+const PORT = process.env.PORT || 8080;
+
 app.use(session({
   secret: 'cxyb5OVktj6baDGKpsZzRvpcStrDTsR9',
   resave: false,
   saveUninitialized: true,
   cookie: { secure: false },
 }));
+
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.set("view engine", "ejs");
-app.use('/', genRoutes);
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: false }));
+
+app.use('/', genRoutes);
 app.use(userRoutes);
 
-
 app.listen(PORT, () => {
-    console.log("App listening at: http://localhost:8080/");
-  });
+    console.log(`App listening at: http://localhost:${PORT}/`);
+});
+
--- Etel_projekt/routes/gen_routes.js
+++ Etel_projekt/routes/gen_routes.js
@@ -1,42 +1,44 @@
 const express = require("express");
 const router = express.Router();
-const jwt = require("jsonwebtoken");
 
+router.use((req, res, next) => {
+    const validRoutes = ['/login', '/register', '/reserve', '/menu', '/reviews', '/contact', '/'];
+    if (!validRoutes.includes(req.path)) {
+        return res.redirect('/');
+    }
+    next();
+});
 
-router.get('/', (req, res)=>{
+router.get('/', (req, res) => {
     return res.render('home');
 });
 
-router.get('/login', (req, res)=>{
-    return res.render('login');
+router.get('/login', (req, res) => {
+    return res.render('login'); 
 });
 
-router.get('/register', (req, res)=>{
+router.get('/register', (req, res) => {
     return res.render('registration');
 });
 
 router.get('/reserve', (req, res) => {
     if (req.session.user) {
-        return res.render('reserve');
+        return res.render('reserve'); 
     } else {
-        return res.render('login');
+        return res.redirect('/login'); 
     }
 });
 
-router.get('/menu', (req, res)=>{
-    return res.render('menu');
+router.get('/menu', (req, res) => {
+    return res.render('menu'); 
 });
 
-router.get('/reviews', (req, res)=>{
+router.get('/reviews', (req, res) => {
     return res.render('reviews');
 });
 
-router.get('/contact', (req, res)=>{
+router.get('/contact', (req, res) => {
     return res.render('contact');
-});
-
-router.get('*', (req, res) => {
-    return res.render('home');
 });
 
 module.exports = router;
