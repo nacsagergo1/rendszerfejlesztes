5ffb080cf21345ce0d5f84094b268017ac726dd7
Sánta Krisztina Csilla <h269825@stud.u-szeged.hu>
2024-11-16 19:20:11 +0100

    add password reset button

--- Etel_projekt/config/db.js
+++ Etel_projekt/config/db.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
   host: 'localhost',
   port: 3306,
-  user: 'projectg_root',  
-  password: 'VpvUhR36YiGT', 
+  user: 'projectg_root', 
+  password: 'VpvUhR36YiGT',
   database: 'projectg_Asztafoglalos3',  
   connectionLimit: 10 
 }).promise();
