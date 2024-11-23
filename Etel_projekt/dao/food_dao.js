1920db59e13f107d778a0ee6950f8ad94428b5eb
Sánta Krisztina Csilla <h269825@stud.u-szeged.hu>
2024-11-23 20:24:46 +0100

    add food dao

--- Etel_projekt/dao/food_dao.js
+++ Etel_projekt/dao/food_dao.js
@@ -3,27 +3,126 @@ const db = require('../config/db');
 class FoodDAO{
 
     async getFoods(){
-        //TODO
+        try{
+            const [foods] = await db.query('SELECT * FROM food');
+            return foods;
+        } catch (error){
+            console.log("Hiba az ételek lekérdezésekor.", error);
+            return null;
+        }
     }
 
     async getFoodsByCategory(category) {
-        //TODO
+        try{
+            const [categoryFoods] = await db.query('SELECT * FROM food WHERE Category = ?', [category]);
+            return categoryFoods;
+        } catch (error) {
+            console.log("Hiba az ételek kategória szerinti lekérdezésekor.", error);
+            return null;
+        }
+        
     }
 
     async deleteFood(foodId){
-        //TODO
+        try{
+            const [result] = await db.query('DELETE FROM food WHERE ID = ?', [foodID]);
+
+            if(result && result.affectedRows === 1){
+                return true;
+            } else {
+                return false;
+            }
+        } catch (error){
+            console.log("Hiba az étel törlésekor: ", error);
+            return false;
+        }
     }
 
     async deleteFoodFromMenu(foodID) {
-        //TODO
+        try{
+            const [result] = await db.query('DELETE FROM food_menus WHERE Food_ID = ?', [foodID]);
+
+            if(result){
+                return true;
+            } else {
+                return false;
+            }
+        } catch (error){
+            console.log("Hiba a törléskor: ", error);
+            return false;
+        }
     }
 
     async createFood(name, description, category, price, image_path){
-        //TODO
+        const [existing] = await db.query('SELECT * FROM food WHERE ID = ?', [foodID]);
+
+        if(existing.length > 0){
+            return false;
+        } else {
+            try{
+                const [result] = await db.query('INSERT INTO food (Name, Description, Category, Price, Image_path) VALUES (?,?,?,?,?)', [name, description, category, price, image_path]);
+
+                if(result && result.affectedRows === 1){
+                    return true;
+                } else {
+                    return false;
+                }
+            } catch (error){
+                console.log("Hiba az étel mentésekor: ", error);
+                return false;
+            }
+        }
     }
 
     async getMenus(){
-        //TODO
+        const query = `SELECT m.ID AS menu_id, m.Name AS menu_name, m.Active_From AS menu_from, m.Active_Until AS menu_until, 
+                        f.ID AS food_id, f.Name AS food_name, f.Description AS food_description, f.Category AS food_category, f.Price AS food_price, f.Imge_path AS food_image
+                        FROM menus m
+                        LEFT JOIN food_menus fm ON m.ID = fm.Menu_ID
+                        LEFT JOIN food f ON fm.Food_ID = f.ID
+                        ORDER BY m.ID, f.ID;`;
+        
+        try{
+            const [rows] = await db.query(query);
+
+            if(rows){
+                const menus = {};
+
+                rows.forEach(row => {
+                    const menuID = row.menu_id;
+
+                    if(!menus[menuID]){
+                        menus[menuID] = {
+                            menu_id: menuID,
+                            name: row.menu_name,
+                            active_from: row.menu_from,
+                            active_until: row.menu_until,
+                            foods: []
+                        };
+                    }
+
+                    if(row.food_id){
+                        menus[menuID].foods.push({
+                            food_id: row.food_id,
+                            name: row.food_name,
+                            description: row.food_description,
+                            category: row.food_category,
+                            price: row.food_price,
+                            image: row.food_image
+                        });
+                    }
+                });
+
+                return Object.values(menus);
+
+            } else {
+                console.log("Hiba a menük lekérdezésekor.");
+                return null;
+            }
+        } catch (error){
+            console.log("Hiba a menük lekérdezésekor: ", error);
+        }
+                        
     }
 
 };
