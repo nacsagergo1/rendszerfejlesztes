e5cf9f8a85dc86e29a2af5e32a7f1059c26ea2cf
Varga Csanád Sándor <h379029@stud.u-szeged.hu>
2024-11-27 17:19:02 +0000

    Replace db.js

--- Etel_projekt/config/db.js
+++ Etel_projekt/config/db.js
@@ -3,8 +3,10 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
   host: 'localhost',
   port: 3306,
-  user: 'root', 
-  database: 'projectg_asztalfoglalos3',
+  user: 'projectg_root',  
+  password: 'VpvUhR36YiGT', 
+  database: 'projectg_Asztafoglalos3',  
+  connectionLimit: 10 
 }).promise();
 
 module.exports = pool;
